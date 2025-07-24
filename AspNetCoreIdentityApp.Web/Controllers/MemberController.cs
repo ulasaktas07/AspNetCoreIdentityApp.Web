57e651edbcf8d5b9f4ebae24cc22e3e10997f887
Ulaş Aktaş <ulasaktas882@gmail.com>
2025-07-24 14:57:21 +0300

    Member Sayfasını Tasarlanması

--- AspNetCoreIdentityApp.Web/Controllers/MemberController.cs
+++ AspNetCoreIdentityApp.Web/Controllers/MemberController.cs
@@ -1,17 +1,24 @@
 ﻿using AspNetCoreIdentityApp.Web.Models;
+using AspNetCoreIdentityApp.Web.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
-
 namespace AspNetCoreIdentityApp.Web.Controllers
 {
 	[Authorize]
-	public class MemberController(SignInManager<AppUser> signInManager) : Controller
+	public class MemberController(SignInManager<AppUser> signInManager,UserManager<AppUser> userManager) : Controller
 	{
-		public IActionResult Index()
+		public async Task<IActionResult> Index()
 		{
-			return View();
+			var currentUser = await userManager.FindByNameAsync(User.Identity!.Name!);
+			var userViewModel = new UserViewModel
+			{
+				UserName = currentUser!.UserName,
+				Email = currentUser.Email,
+				PhoneNumber = currentUser.PhoneNumber
+			};
+			return View(userViewModel);
 		}
 		public async Task Logout()
 		{
