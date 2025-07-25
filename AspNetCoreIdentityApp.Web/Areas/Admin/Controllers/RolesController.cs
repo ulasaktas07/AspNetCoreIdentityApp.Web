cf387e325ddc1eba001ee10d1d6438b2248926e5
Ulaş Aktaş <ulasaktas882@gmail.com>
2025-07-25 16:15:27 +0300

    Role Listing, Update and Deletion Operations

--- AspNetCoreIdentityApp.Web/Areas/Admin/Controllers/RolesController.cs
+++ AspNetCoreIdentityApp.Web/Areas/Admin/Controllers/RolesController.cs
@@ -3,18 +3,24 @@ using AspNetCoreIdentityApp.Web.Extensions;
 using AspNetCoreIdentityApp.Web.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 
 namespace AspNetCoreIdentityApp.Web.Areas.Admin.Controllers
 {
 	[Area("Admin")]
 
-	public class RolesController(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager) : Controller
+	public class RolesController( RoleManager<AppRole> roleManager) : Controller
 	{
-		public IActionResult Index()
+		public async Task<IActionResult> Index()
 		{
+			var roles = await roleManager.Roles.Select(r => new RoleViewModel
+			{
+				Id = r.Id,
+				Name = r.Name!
+			}).ToListAsync();
 
-			return View();
+			return View(roles);
 		}
 		public IActionResult RoleCreate()
 		{
@@ -30,6 +36,49 @@ namespace AspNetCoreIdentityApp.Web.Areas.Admin.Controllers
 				ModelState.AddModelErrorList(result.Errors);
 				return View();
 			}
+
+			TempData["SuccessMessage"] = "Rol başarıyla oluşturuldu.";
+
+			return RedirectToAction(nameof(RolesController.Index));
+		}
+
+		public async Task<IActionResult> RoleUpdate(string id)
+		{
+			var role = await roleManager.FindByIdAsync(id);
+			if (role == null)
+			{
+				throw new Exception("Rol bulunamadı");
+			}
+
+			return View(new RoleUpdateViewModel() { Id = role.Id, Name = role.Name! });
+		}
+		[HttpPost]
+		public async Task<IActionResult> RoleUpdate(RoleUpdateViewModel request)
+		{
+			var role = await roleManager.FindByIdAsync(request.Id) ?? throw new Exception("Rol bulunamadı");
+			role!.Name = request.Name;
+			var result = await roleManager.UpdateAsync(role);
+		
+			if (!result.Succeeded)
+			{
+				ModelState.AddModelErrorList(result.Errors);
+				return View();
+			}
+			ViewData["SuccessMessage"] = "Rol başarıyla güncellendi.";
+			return View();
+		}
+		public async Task<IActionResult> RoleDelete(string id)
+		{
+			var roleDelete = await roleManager.FindByIdAsync(id) ?? throw new Exception("Rol bulunamadı");
+
+			var result = await roleManager.DeleteAsync(roleDelete);
+			if (!result.Succeeded)
+			{
+				throw new Exception(result.Errors.Select(x=>x.Description).First());
+			}
+
+			TempData["SuccessMessage"] = "Rol başarıyla silindi.";
+
 			return RedirectToAction(nameof(RolesController.Index));
 		}
 	}
--- AspNetCoreIdentityApp.Web/Areas/Admin/Models/RoleUpdateViewModel.cs
+++ AspNetCoreIdentityApp.Web/Areas/Admin/Models/RoleUpdateViewModel.cs
@@ -2,10 +2,12 @@
 
 namespace AspNetCoreIdentityApp.Web.Areas.Admin.Models
 {
-	public class RoleCreateViewModel
+	public class RoleUpdateViewModel
 	{
+		public string Id { get; set; } = null!;
+
 		[Required(ErrorMessage = "Rol isim  boş bırakılamaz")]
 		[Display(Name = "Rol Adı")]
-		public string Name { get; set; }=null!;
+		public string Name { get; set; } = null!;
 	}
 }
